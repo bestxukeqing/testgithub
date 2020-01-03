e797396f0da53c3348bc253fb01e978417c5cee7
bestxukeqing <57779514+bestxukeqing@users.noreply.github.com>
2020-01-03 10:57:48 +0800

    Update Test.java

--- TestGitHub/src/com/xu/test/Test.java
+++ TestGitHub/src/com/xu/test/Test.java
@@ -3,6 +3,7 @@ package com.xu.test;
 public class Test {
 
 	public static void main(String[] args) {
-		System.out.println("Test.main()");
+		int i = 0;
+		System.out.println("Test.main()"+i);
 	}
 }
